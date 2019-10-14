1b43cfc8ff1e10e5b5bf2f094e238a032cf5a0f7
Nymmh <stickman7415web@gmail.com>
2019-10-14 17:49:22 -0600

    Added global system msg when weather is updated

--- handlers/weatherUpdates.js
+++ handlers/weatherUpdates.js
@@ -1,8 +1,10 @@
-let weather = require('../closed/weather.js');
+let weather = require('../closed/weather.js'),
+    handleSocket = require('../handlers/handleSocket.js');
 module.exports = {
     sendWeatherUpdate(SOCKET_LIST,weatherType,roundTemp,randClouds,roundedWind,roundHumidity){
         for(let i in SOCKET_LIST){
             SOCKET_LIST[i].emit('updateWeather',{type:weatherType,temp:roundTemp,clouds:randClouds,wind:roundedWind,humidity:roundHumidity});
+            SOCKET_LIST[i].emit('addToChatGlobal',`System: A new weather report has come in.`);
         }
     },
     requestWeather(socket){
